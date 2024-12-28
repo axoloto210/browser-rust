a6d9a501ef528b456824e0319c288c851129b5be
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-12-29 04:43:59 +0900

    URL parse

--- browser-rust-core/src/url.rs
+++ browser-rust-core/src/url.rs
@@ -1,4 +1,7 @@
-use alloc::string::{String, ToString};
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Url {
@@ -11,12 +14,93 @@ pub struct Url {
 
 impl Url {
     pub fn new(url: String) -> Self {
-        Url {
+        Self {
+            //SelfはImplで指定している構造体の型
             url,
             host: "".to_string(),
             port: "".to_string(),
             path: "".to_string(),
             searchpart: "".to_string(),
         }
+    }
+
+    //&mut self でオブジェクトのプロパティを変更可能に
+    pub fn parse(&mut self) -> Result<Self, String> {
+        if !self.is_http() {
+            return Err("Only http schema allowed.".to_string());
+        }
+
+        self.host = self.extract_host();
+        self.port = self.extract_port();
+        self.path = self.extract_path();
+        self.searchpart = self.extract_searchpart();
+
+        Ok(self.clone())
+    }
+
+    fn is_http(&mut self) -> bool {
+        return self.url.contains("http://");
+    }
+
+    fn extract_host(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        //ポート番号の有無を判定
+        //Some型は値が存在することを明示する型。コンパイラが型チェックを行うので安全。
+        if let Some(index) = url_parts[0].find(':') {
+            //':'よりも前の文字を結合して返す。[..index]はRustのスライス構文で、文字列の部分参照（スライス）を作成。
+            //'foo:8000'であれば、0番目からindex-1 番目のスライス'foo'を取得。マルチバイト文字は意図通りに分割できない（バイト単位）ため注意。
+            url_parts[0][..index].to_string()
+        } else {
+            url_parts[0].to_string()
+        }
+    }
+
+    fn extract_port(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][index + 1..].to_string()
+        } else {
+            "80".to_string()
+        }
+    }
+
+    fn extract_path(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        path_and_searchpart[0].to_string()
+    }
+
+    fn extract_searchpart(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        path_and_searchpart[1].to_string()
     }
 }
