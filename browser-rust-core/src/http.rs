3e0913156ffaec7045ffa5f241a289de1f3e330a
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-12-29 10:57:07 +0900

    add http parser

---
+++ browser-rust-core/src/http.rs
@@ -0,0 +1,90 @@
+use alloc::fmt::format;
+use alloc::format;
+use alloc::string::{String, ToString};
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone)]
+pub struct HttpResponse {
+    version: String,
+    stasu_code: u32,
+    reason: String,
+    headers: Vec<Header>,
+    body: String,
+}
+
+impl HttpResponse {
+    pub fn new(raw_response: String) -> Result<Self, Error> {
+        let preprocessed_response = raw_response.trim_start().replaace("\n\r", "\n");
+
+        let (status_line, remaining) = match preprocessed_response.split_once("\n") {
+            Some((s, r)) => (s, r),
+            None => {
+                return Err(Error::Network(
+                    "invalid http response: {}",
+                    preprocessed_response,
+                ));
+            }
+        };
+
+        let (headers, body) = match remaining_split.once("\n\n") {
+            Some((h, b)) => {
+                let mut headers = Vec::new();
+                for header in h.split('\n') {
+                    let splitted_header: Vec<&str> = header.splitn(2, ':').collect();
+                    headers.push(Header::new(
+                        String::from(splitted_header[0].trim()),
+                        String::from(splitted_header[1].trim()),
+                    ));
+                }
+                (headers, b)
+            }
+            None => (Vec::new(), remaining),
+        };
+
+        let statuses: Vec<&str> = status_line.split(' ').collect();
+
+        Ok(Self {
+            version: statuses[0].to_string(),
+            stasu_code: statuses[1].parse().unwrap_or(404),
+            reason: statuses[2].to_string(),
+            headers,
+            body: body.to_string(),
+        })
+    }
+
+    pub fn version(&self) -> String {
+        self.version.clone()
+    }
+    pub fn stasu_code(&self) -> u32 {
+        self.stasu_code.clone()
+    }
+    pub fn reason(&self) -> String {
+        self.reason.clone()
+    }
+    pub fn headers(&self) -> Vec<Header> {
+        self.headers.clone()
+    }
+    pub fn body(&self) -> String {
+        self.body.clone()
+    }
+
+    pub fn header_value(&self, name: &str) -> Result<String, String> {
+        for h in &self.headers {
+            if h.name == name {
+                return Ok(h.value.clone());
+            }
+        }
+        Err(format!("failed to find {} in headers", name))
+    }
+}
+
+pub struct Header {
+    name: String,
+    value: String,
+}
+
+impl Header {
+    pub fn new(name: String, value: String) -> Self {
+        Self { name, value }
+    }
+}
