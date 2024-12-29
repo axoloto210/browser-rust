7d3f3cbb26c71c86dc65642811c325eda6fd343b
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-12-30 07:01:57 +0900

    example.comへアクセス

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-#![no_std]
+#![no_std]  // 標準ライブラリを使用しない
+#![no_main] // 標準のmain関数エントリーポイントを使用しない
 #![cfg_attr(not(target_os = "linux"), no_main)]
+
+extern crate alloc; //no_stdのため、明示的にallocを導入する必要がある
+
+use alloc::string::ToString;
+use net_wasabi::http::HttpClient;
 
 use noli::prelude::*;
 
 fn main() {
-    Api::write_string("Hello World\n");
-    println!("Hello from println!");
-    Api::exit(42);
+    let client = HttpClient::new();
+    match client.get("example.com".to_string(), 80, "/".to_string()){
+        Ok(res)=>{
+            print!("response:\n{:#?}",res);
+
+        }
+        Err(e)=>{
+            print!("error:\n{:#?}",e)
+        }
+    }
 }
 
 entry_point!(main);
